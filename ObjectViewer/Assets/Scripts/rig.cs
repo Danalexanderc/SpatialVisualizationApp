c628f31750b6f55b19c2d7a504137296a7d8147d
Danalexanderc <31372021+Danalexanderc@users.noreply.github.com>
2020-05-28 09:46:06 -0400

    Initial
    Currently contains a menu, the basic plane cutter, and 2D projection of a cube.

---
+++ ObjectViewer/Assets/Scripts/rig.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rig : MonoBehaviour
+{
+    public Transform t;
+
+    public float rotX = 0f, rotY = 0f, rotZ = 0f, rotationDelta = 7;
+
+    public Vector3 defaultScale = new Vector3();
+
+    private Vector3 rotation = new Vector3(), scaleChange = new Vector3();
+
+    // Using FixedUpdate instead of Update because I am using physics
+    void FixedUpdate()
+    {
+        rotationDelta = 4;
+        defaultScale.Set(0.5f, 0.5f, 0.5f);
+
+        // Rotation controls
+        if (Input.GetKey("u"))
+            rotX += rotationDelta;
+        if (Input.GetKey("j"))
+            rotX -= rotationDelta;
+        if (Input.GetKey("i"))
+            rotY += rotationDelta;
+        if (Input.GetKey("k"))
+            rotY -= rotationDelta;
+        if (Input.GetKey("o"))
+            rotZ += rotationDelta;
+        if (Input.GetKey("l"))
+            rotZ -= rotationDelta;
+
+        if (Input.GetKey("r")) // Reset scale and rotation.
+        { rotX = 0; rotY = 0; rotZ = 0;
+            t.localScale = defaultScale;
+        }
+
+        // Scale controls
+        if (Input.GetKey("y"))
+            scaleChange.Set(0.01f, 0.01f, 0.01f);
+        else if (Input.GetKey("h"))
+            scaleChange.Set(-0.01f, -0.01f, -0.01f);
+        else
+            scaleChange.Set(0f, 0f, 0f);
+
+        t.localScale += scaleChange;
+        rotation.Set(rotX, rotY, rotZ);
+        t.localEulerAngles = rotation;
+
+    }
+}
