3502a848b7f8d507c771e1a3c38fff843f68b56f
Danalexanderc <31372021+Danalexanderc@users.noreply.github.com>
2020-06-30 09:20:57 -0400

    Partial object maker
    Added a partially functioning object maker. Also added another challenge for object viewer: copy animation.

--- ObjectViewer/Assets/Scripts/ObjectViewer/ObjectManager.cs
+++ ObjectViewer/Assets/Scripts/ObjectViewer/ObjectManager.cs
@@ -19,7 +19,7 @@ public class ObjectManager : MonoBehaviour
 	
     void Update(){
 		for(int i = 0; i < keys.Length; i++){ // Checks if the user has released any of the number keys and sets the active object based on that.
-			if(Input.GetKeyUp(keys[i])){
+			if(Input.GetKeyUp(keys[i]) && i < objects.Length){
 				int temp = i;
 				
 				if(Input.GetKey(KeyCode.Space) && i + 9 < objects.Length) // Holding the spacebar allows the user to select other objects with the number keys.
